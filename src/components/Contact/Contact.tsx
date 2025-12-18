4af29599baeeac85a75e6bf1bb87fcb7e92f99a8
Dhruv Tikhande <itcs95123@gmail.com>
2025-12-18 12:29:19 +0530

    Checking the contact section error

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -24,13 +24,6 @@ const Contact: React.FC = () => {
     }
 
     setLoading(true);
-
-    console.log(
-      'ENV CHECK:',
-      import.meta.env.VITE_EMAILJS_SERVICE_ID,
-      import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
-      import.meta.env.VITE_EMAILJS_PUBLIC_KEY
-    );
     
     emailjs
       .send(
